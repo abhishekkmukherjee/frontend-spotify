5e9b237ae9227ab043832071d7dea7824043981e
Abhishek Mukherjee <mukherjeeabhishek207@gmail.com>
2022-05-12 15:59:22 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// every single html element must be between the div
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <p className='text-danger'>
+    Hello World</p>
+    <h1 className='text-danger'>
+    Spotify App</h1>
     </div>
   );
 }
